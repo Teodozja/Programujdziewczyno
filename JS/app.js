d3cf753be02775dc0f10c0ca68610aa041ad4e1a
Teodozja <teodozja6@gmail.com>
2017-10-21 17:14:59 +0200

    Zadanie_2

--- JS/app.js
+++ JS/app.js
@@ -18,4 +18,28 @@ document.addEventListener('DOMContentLoaded',function(){
     dropdownMenu.style.display="none";
   });
 
+  var buttons = document.querySelectorAll('.read-more');
+  console.log(buttons);
+  console.log(buttons[0].previousElementSibling);
+
+  //Zadanie 2
+
+  buttons[0].addEventListener('click', showHide);
+
+    function showHide(){
+    var textArea = this.previousElementSibling;
+      if(textArea.style.display === 'none' || textArea.style.display === '') {
+
+          textArea.style.display = 'block';
+          this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+      } else {
+        textArea.style.display = 'none';
+        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>'
+  }
+
+  };
+
+  for (var i=0; i<buttons.length; i++) {
+    buttons[i].addEventListener('click',showHide);
+  }
 });
